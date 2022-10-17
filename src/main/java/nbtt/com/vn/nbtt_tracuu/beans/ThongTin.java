d4bf60cb76f5b687712d060631061c6f22aba0b5
DANGLONG <long.bmw10@gmail.com>
2022-10-18 02:53:23 +0900

    da tao 2 file du lieu

---
+++ src/main/java/nbtt/com/vn/nbtt_tracuu/beans/ThongTin.java
@@ -0,0 +1,19 @@
+package nbtt.com.vn.nbtt_tracuu.beans;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ThongTin {
+    
+    private Integer sTT; // 順番
+    private String ten; // 名
+    private String hangSanXuat; // 製
+    private Integer gia; // 価格
+    private String tacDung; // 用
+    private Integer tonKho; // 残り
+    NhanHieu nhanHieu;
+}
--- src/main/java/nbtt/com/vn/nbtt_tracuu/controller/NbttController.java
+++ src/main/java/nbtt/com/vn/nbtt_tracuu/controller/NbttController.java
@@ -1,13 +1,30 @@
 package nbtt.com.vn.nbtt_tracuu.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import nbtt.com.vn.nbtt_tracuu.beans.NhanHieu;
+import nbtt.com.vn.nbtt_tracuu.beans.ThongTin;
 
 @Controller
 public class NbttController {
     
     @GetMapping("/Tracuu")
-    public String showTracuu(){
+    public String showTracuu(Model model){
+
+        List <NhanHieu> nhanHieus = new ArrayList<>();
+        nhanHieus.add(new NhanHieu("R1", "Rượu"));
+        
+        List <ThongTin> thongTins = new ArrayList<>();
+        thongTins.add(new ThongTin(1, "Yamayaki", "yama", 2222222, "#", 1, new NhanHieu("R1", "Rượu")));
+        
+        model.addAttribute("thongTins", thongTins);
+        model.addAttribute("nhanHieus", nhanHieus);
+
         return "Tracuu";
     }
 }
